b56b8238dff326f6aa9fca144fc2a0ce93711f56
Mateusz-Latka <115552649+Mateusz-Latka@users.noreply.github.com>
2022-12-15 17:56:37 +0100

    Creating Migration File
    We will create laravel migration file using the below command.
    
    php artisan make:migration create_people_table

---
+++ laravel/RESTAPI/database/migrations/2022_12_15_164558_create_people_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('FIRST NAME', 255);
+            $table->string('SECOND NAME', 255);
+            $table->integer('TELEPHONE NUMBER');
+            $table->string('RESIDENCE ADDRESS', 255);
+            $table->string('COUNTRY', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+};
