751a6142dc2e710400d3c9094b80de8b6805c797
Mateusz-Latka <115552649+Mateusz-Latka@users.noreply.github.com>
2022-12-15 19:45:14 +0100

    Update the routes
    adding the correct routes

--- laravel/RESTAPI/routes/api.php
+++ laravel/RESTAPI/routes/api.php
@@ -2,18 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PeopleController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('people',[PeopleController::class, 'index']);
+Route::get('people/{id}',[PeopleController::class, 'show']);
+Route::delete('people/{id}', [AuthorController::class, 'delete']);
+Route::post('people', [AuthorController::class, 'create']);
+Route::put('people/{id}', [AuthorController::class, 'update']);
